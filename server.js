7a29c15adc0817628a533e9a7bfe0f8eafaa3d3b
Tri Ramdhani <triaresraamdhani@gmail.com>
2023-05-16 00:20:08 +0700

    feat: configure dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
@@ -7,7 +8,7 @@ const RouterBiodata = require('./route/routeBidodata')
 const logEvent = require('./middleware/logEvent')
 const corsOption = require('./config/corsOption')
 
-
+const port = process.env.PORT ||
 
 app.use(logEvent)
 app.use(express.json())
@@ -17,10 +18,10 @@ app.use(cors(corsOption))
 app.use('/biodata', RouterBiodata)
 
 app.get('/', (req,res)=> {
-    res.send('server running on port 3500')
+    res.send('Coded by Tri Ramdhani')
 })
 
 
 app.listen(3500 , ()=>{
-    console.log('Server running on port 3500')
+    console.log('Server running at http://localhost:' + port)
 })
